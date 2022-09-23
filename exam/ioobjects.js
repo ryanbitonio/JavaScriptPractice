03649ffe759c32e55c88712739d742d7fb7e62d6
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2022-09-23 08:47:48 +0800

    JavaScript Essentials : Exercise 3 - Objects

---
+++ exam/ioobjects.js
@@ -0,0 +1,19 @@
+// Import readline-sync
+const readlineSync = require("readline-sync");
+const objects = [];
+
+// Do three times
+for (let i = 0; i < 3; i++) {
+  const object = {};
+  object.fruit = readlineSync.question("Enter a fruit: ");
+  object.color = readlineSync.question("Enter a color: ");
+  objects.push(object);
+}
+
+let input = readlineSync.question("Select a fruit to display: ");
+for (let i = 0; i < objects.length; i++) {
+  // Checks if the input is the fruit in the object
+  if (input === objects[i].fruit) {
+    console.log(`The color of ${objects[i].fruit} is ${objects[i].color}.`);
+  }
+}
