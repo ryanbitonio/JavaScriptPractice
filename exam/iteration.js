aa708bbf9f872f400eb2c2edeaeb4076854db0c0
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2022-10-04 14:57:32 +0800

    Creating iteration in Exercise 2

--- exam/iteration.js
+++ exam/iteration.js
@@ -1,6 +1,6 @@
 const persons = [
   { firstName: "Naruto", lastName: "Uzumaki", age: 10 },
-  { firstName: "Sasuke", lastName: "Uchiha", age: 15 },
+  { firstName: "sasuke", lastName: "Uchiha", age: 15 },
   { firstName: "Shikamaru", lastName: "Nara", age: 17 },
   { firstName: "Sakura", lastName: "Haruno", age: 55 },
 ];
@@ -11,3 +11,13 @@ persons.forEach((value) => {
   let ages = value.age;
   console.log(`${first} ${last}, age ${ages}`);
 });
+
+let mapResult = persons.map((persons) => persons.lastName);
+console.log(mapResult);
+
+let findResult = persons.find(
+  // (persons) => persons.firstName.slice(0, 1) === "S"
+  (persons) =>
+    persons.firstName.charAt(0) === "S" || persons.firstName.charAt(0) === "s"
+);
+console.log(findResult);
